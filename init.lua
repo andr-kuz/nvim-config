5cb9b2fbbbcdc0d0e6ba78462aaf1161566347a5
Андрей Кузнецов <awakum192@gmail.com>
2023-04-28 22:28:49 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
--- nodejs npm xclip unzip fd
+-- nodejs npm xclip unzip fd fzf
 -- pip3 install neovim pylint
 -- install all these before neovim
 -- Store in ~/.config/nvim/
 -- Plugins stored in ~/.local/share/nvim/
+
 -- From https://github.com/nvim-lua/kickstart.nvim/raw/master/init.lua
 -- Review https://www.youtube.com/watch?v=stqUbv-5u2s
 
