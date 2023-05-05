8201c551f431ca9ba1b67ce726f0c0a97e464985
Андрей Кузнецов <awakum192@gmail.com>
2023-05-05 21:55:02 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -478,7 +478,7 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Setup mason so it can manage external tooling
 require('mason').setup {
-  ensure_installed = {"pylsp"},
+  ensure_installed = {"pylsp", "pyright"},
 }
 
 -- Ensure the servers above are installed
